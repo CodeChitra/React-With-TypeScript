90a7e3e127f0b2bc4e58d7ba5ba3b0cfd1a548d5
Akash Deep Chitransh <codechitra@gmail.com>
2024-04-17 19:49:09 +0530

    forwardRef

--- project-2/src/App.tsx
+++ project-2/src/App.tsx
@@ -1,6 +1,5 @@
 // import Button from "./components/Button";
 import Container from "./components/Container";
-// import Input from "./components/Input";
 
 function App() {
   return <Container as="button">Button</Container>;
