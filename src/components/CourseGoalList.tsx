d51531b7f7915d0f09599718ecc2b6389ce85a1a
Akash Deep Chitransh <codechitra@gmail.com>
2024-04-17 18:28:00 +0530

    Learn Discriminated Union

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,21 +1,38 @@
+import { ReactNode } from "react";
 import { type CourseGoalType } from "../App";
 import CourseGoal from "./CourseGoal";
+import InfoBox from "./InfoBox";
 
 type CourseGoalListProps = {
   goals: CourseGoalType[];
   onDeleteGoal: (id: number) => void;
 };
 const CourseGoalList = ({ goals, onDeleteGoal }: CourseGoalListProps) => {
+  if (goals.length === 0) {
+    return <InfoBox mode="hint">You have no goals yet!</InfoBox>;
+  }
+
+  let warningBox: ReactNode;
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="medium">
+        You are collecting a lot of goals!
+      </InfoBox>
+    );
+  }
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
